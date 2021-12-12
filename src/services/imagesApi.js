29af3e37acfaea3b09d532c7f35854ef644f1eea
Viktor Kostiuchenko <kostiuchenkoviktor@gmail.com>
2021-12-13 02:42:40 +0300

    Change fetch function

--- src/services/imagesApi.js
+++ src/services/imagesApi.js
@@ -1,12 +1,31 @@
-function fetchImages(name, page) {
-  return fetch(
-    `https://pixabay.com/api/?q=${name}&page=${page}&key=23833327-aee66bbf86a23c3fb1d188dcb&image_type=photo&orientation=horizontal&per_page=12`,
-  ).then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    return Promise.reject(new Error(`Нет картинок по запросу ${name}`));
+async function fetchImages(name, page) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const KEY = '23833327-aee66bbf86a23c3fb1d188dcb';
+  const searchParams = new URLSearchParams({
+    q: name,
+    per_page: 12,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    page: page,
   });
+
+  let url = `${BASE_URL}?key=${KEY}&${searchParams}`;
+
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    alert(`Нет картинок по запросу ${name}`);
+  }
+
+  //? the way to fetch images just by fetch
+  // return fetch('url').then(response => {
+  //   if (response.ok) {
+  //     return response.json();
+  //   }
+  //   return Promise.reject(new Error(`Нет картинок по запросу ${name}`));
+  // });
+  // ?
 }
 
 const api = {
